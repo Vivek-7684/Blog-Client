3e0f383045032f195031ddda6f2e57a7628b611a
Vivek-7684 <vivek.infosware@gmail.com>
2025-11-25 13:23:48 +0530

    Real time Error Handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,56 @@ import Stack from '@mui/material/Stack';
 import TextFeid from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { loginSchema } from '../validation';
+import { api } from '../api/api';
+import { useState } from 'react';
 
 export default function Login() {
+
+    const [form, setForm] = useState({}); // form data
+
+    const [alert, setAlert] = useState({ open: false, severity: "", message: "" });
+
+    const [error, setError] = useState({});
+
+    const handleChange = (e) => {
+        // clear blank space
+        if (e.target.value.trimStart() === "") {
+            e.target.value = "";
+        }
+
+        // clear error message with user clear input
+
+        if (e.target.value.trim() === "") {
+            const newErrors = { ...error };
+
+            delete newErrors[e.target.name];
+
+            setError(newErrors);
+
+            // No Validation
+            return;
+        }
+
+        setForm({ ...form, [e.target.name]: e.target.value });
+
+        const result = loginSchema.safeParse(form);
+
+
+        console.log(result);
+
+        if (!result.success) {
+            console.log(result.error.flatten().fieldErrors);
+            setError(result.error.flatten().fieldErrors);
+            return;
+        }
+
+
+    }
+
+    console.log(error?.email?.join("."));
+    console.log(error?.password?.join("."));
+
     return (
         <Stack sx={{ width: '100vw' }} alignItems={'center'} justifyContent={'center'} height={'100vh'}>
             <Stack
@@ -12,8 +60,8 @@ export default function Login() {
                 alignItems={'center'}
                 justifyContent={'center'}>
                 <Typography variant='h4' sx={{ fontSize: "24px", fontWeight: '700' }}>Admin Login</Typography>
-                <TextFeid label="Email" fullWidth variant="outlined" type="email" />
-                <TextFeid label="Password" fullWidth type="password" variant="outlined" />
+                <TextFeid label="Email" error={error?.email?.join()} value={form?.email || ""} helperText={error?.email?.join(".")} name="email" fullWidth variant="outlined" type="email" onChange={(e) => { setForm({ ...form, [e.target.name]: e.target.value }); handleChange(e); }} />
+                <TextFeid label="Password" error={error?.password?.join()} value={form?.password || ""} helperText={error?.password?.join(".")} name="password" fullWidth type="password" variant="outlined" onChange={(e) => { setForm({ ...form, [e.target.name]: e.target.value }); handleChange(e); }} />
                 <Button type="submit" sx={{ width: "100%" }} variant='contained'>Login</Button>
             </Stack>
         </Stack>
---
+++ src/validation.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address.Please Proivide a valid email."),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
