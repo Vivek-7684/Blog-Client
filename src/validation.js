b6e2f5da914bd05e193d7a01984a6348b97300e2
Vivek-7684 <vivek.infosware@gmail.com>
2025-11-25 16:41:26 +0530

    Validation.js

--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
-
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email address.Please Proivide a valid email.").optional(),
-  password: z.string().min(8, "Password must be at least 8 characters long").optional(),
+  email: z
+    .string()
+    .email("Invalid email address.Please Provide a valid email.")
+    .optional(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(12, "Password will contain 12 maximum characters")
+    .optional(),
 });
