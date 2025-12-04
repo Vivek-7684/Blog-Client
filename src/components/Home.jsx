51dffd21c4fadaafd275d90046c53c682c98dc6b
Vivek-7684 <vivek.infosware@gmail.com>
2025-12-04 10:51:28 +0530

    get blogs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,42 +4,70 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
-import { data } from "../data";
 import { useState, useEffect } from 'react';
+import { api } from '../api/api';
 
 export default function Home() {
 
-    const [blogs, setBlogs] = useState(data);
+    const [blogs, setBlogs] = useState('');
 
     const [loadmore, setLoadmore] = useState(5);
 
+    useEffect(() => {
+        api.get("blog")
+            .then((response) => {
+                setBlogs(response.data[0]);
+            })
+            .catch((err) => {
+
+                let messages = [];
+
+                if (Array.isArray(err.response.data)) {
+                    messages = err.response.data.map((errmsg) => errmsg.msg);
+                } else {
+                    messages = [err.response.data];
+                }
+
+                setAlert({
+                    show: true,
+                    type: "error",
+                    messages
+                });
+
+                setTimeout(() => {
+                    setAlert({ show: false, type: "", messages: [] });
+                }, 3000);
+
+            });
+    }, [])
+
     const getLoadMore = () => {
-        setLoadmore(prev => Math.min(prev + 5, data.length));
+        setLoadmore(prev => Math.min(prev + 5, blogs.length));
     }
 
-    
+    console.log(blogs);
 
 
     return (
 
         <Box sx={{ bgcolor: 'rgba(255, 255, 255, 1)', width: '100%', overflowX: 'hidden' }}>
 
-            <Typography variant="h4" sx={{ p: 2, fontWeight: '700', color: 'black' }}>Recently Added Blogs</Typography>
+            <Typography variant="h4" sx={{ p: 5, fontWeight: '700', color: 'black' }}>Recently Added Blogs</Typography>
             <Stack direction={"row"} sx={{ p: 3, width: '100vw' }} gap={1} flexWrap={"wrap"}>
 
-                {data.slice(0, loadmore).map((data, index) => {
+                {blogs && blogs.slice(0, loadmore).map((data, index) => {
                     return (
-                        <Paper key={index} sx={{ bgcolor: 'white', width: '30%', height: '30%', p: 1 }} elevation={3}>
-                            <Avatar alt="Blog" src={data.image} variant="square" sx={{ width: "100%", height: "auto" }} />
+                        <Paper key={index} sx={{ bgcolor: 'white', width: '30%', height: '450px', p: 1 }} elevation={3}>
+                            <Avatar alt="Blog" src={`http://localhost:3000/${data.image_url.replace("\\", '/')}`} variant="square" sx={{ width: "100%", height: "auto" }} />
                             <Typography variant="h6" sx={{ p: 1, fontWeight: '700', fontSize: '20px' }}>{data.title}</Typography>
                             <Typography variant="p" sx={{ p: 1, fontSize: '16px', fontWeight: '500' }}>
-                                {data.description}
+                                {data.content.substring(0, 300)}
                             </Typography>
                             <Button sx={{ fontSize: '10px', p: 1 }}>ReadMore</Button>
                         </Paper>
                     )
                 })}
-                {loadmore !== data.length && (<Stack sx={{ width: '100%', alignItems: 'center', justifyContent: 'center', p: 2 }}>
+                {loadmore !== blogs.length && (<Stack sx={{ width: '100%', alignItems: 'center', justifyContent: 'center', p: 2 }}>
                     <Button sx={{ p: 2, color: 'white', bgcolor: 'orange', width: '20%', fontSize: '16px', fontWeight: '700' }} variant='contained' onClick={getLoadMore}>LoadMore...</Button>
                 </Stack>)}
             </Stack>
